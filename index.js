57bab549932311244a5699ca5966c54b1275f2e6
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-09 22:21:55 +0100

    cros index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const axios = require('axios')
 const PORT = 3000
 const GetAPI = async () => {
