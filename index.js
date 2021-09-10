ca7eac15d23cf97888202d30da03d32ade50e8f4
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-10 23:41:40 +0100

    func

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const axios = require('axios')
-app.use(cors())
-const PORT = 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const axios = require("axios");
+app.use(cors());
+const PORT = 3000;
+const SetPaths = (strpaths) => {
+  let path = "";
+  for (let i = 0; i < strpaths.length; i++)
+    if (strpaths[i] === "*") {
+      path += strpaths[i];
+    } else path += strpaths[i];
+};
 
-
-app.get('/:protocol/:url/:domaine/:path', async (req, res) => {
-    let URL = String(req.params.protocol + "://" + req.params.url + "." + req.params.domaine + "/" + req.params.path)
-    const Response = await axios.get(URL)
-    return res.json(Response.data)
-})
-app.listen(process.env.PORT || PORT, () => { console.log(`APP IS RUNNIND : ${PORT}`) })
+app.get("/:protocol/:url/:domaine/:paths", async (req, res) => {
+  let URL = String(
+    req.params.protocol +
+      "://" +
+      req.params.url +
+      "." +
+      req.params.domaine +
+      "/" +
+      req.params.path
+  );
+  const Response = await axios.get(URL);
+  return res.json(Response.data);
+});
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`APP IS RUNNIND : ${PORT}`);
+});
