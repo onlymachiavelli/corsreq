84d4136511b814f8f8ee6fe0d65bb984913242eb
onlymachiavelli <tmakaveli643@gmail.com>
2021-09-10 22:14:53 +0100

    DONE

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const axios = require('axios')
 app.use(cors())
 const PORT = 3000
 
-app.get('/', async (req, res) => {
-    const response = await axios(`http://ip-api.com/json/`).then((res) => res.data)
-    return res.json(response)
+
+app.get('/:protocol/:url/:domaine/:path', async (req, res) => {
+    let URL = String(req.params.protocol + "://" + req.params.url + "." + req.params.domaine + "/" + req.params.path)
+    const Response = await axios.get(URL)
+    return res.json(Response.data)
 })
 app.listen(process.env.PORT || PORT, () => { console.log(`APP IS RUNNIND : ${PORT}`) })
